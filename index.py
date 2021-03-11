bff3c71437a2acd42bba1c07ee7ed28949bd23ac
dpong <powenperng@gmail.com>
2021-03-11 18:25:24 +0800

    Updated.

--- index.py
+++ index.py
@@ -29,6 +29,12 @@ def index_layout():
             dbc.Row([
                 dash_table.DataTable(
                     id='main_table',
+                    page_current=0,
+                    page_size=10,
+                    page_action='custom',
+                    sort_action='custom',
+                    sort_mode='single',
+                    sort_by=[],
                     style_cell={'textAlign': 'center', 'height': 'auto', 'minWidth': '120px', 'width': '120px', 'maxWidth': '120px','whiteSpace': 'normal', 'backgroundColor': 'rgba(0, 0, 0, 0)', 'color':'rgb(211,211,211)'},
                     style_header={
                             'backgroundColor': 'rgb(105,105,105)',
@@ -37,6 +43,7 @@ def index_layout():
                         },
                 ),
                 ], justify="center", style={'margin-top':'5px'}),
+                html.Div(id='storage1_all', style={'display': 'none'}),
         ], width=10, xs=10, lg=6),
         dbc.Col([], width=1, xs=1, lg=3),
     ]),
@@ -50,8 +57,28 @@ def display_page(pathname):
         
 
 @app.callback(
+    Output('main_table', 'data'),
+    [Input('main_table', "page_current"),
+     Input('main_table', "page_size"),
+     Input('main_table', 'sort_by'),
+     Input('storage1_all', 'children')])
+def update_table(page_current, page_size, sort_by, data1):
+    df = pd.read_json(data1, orient='split')
+    if len(sort_by):
+        dff = df.sort_values(
+            sort_by[0]['column_id'],
+            ascending=sort_by[0]['direction'] == 'asc',
+            inplace=False
+        )
+    else:
+        # No sort is applied
+        dff = df
+    return dff.iloc[page_current*page_size:(page_current+ 1)*page_size].to_dict('records')
+
+
+@app.callback(
     [Output('main_table', 'columns'),
-    Output('main_table', 'data')],
+    Output('storage1_all', 'children')],
     [Input('main_drop', 'value')])
 def update_share_data(option):
     limited = 1000
@@ -70,7 +97,7 @@ def update_share_data(option):
         df['Token Insight'] = fcas['ratinga']
         df = df[['symbol', 'name', 'FCAS', 'Simetri', 'Token Insight']]
     col = [{"name": str(i), "id": str(i)} for i in df.columns]
-    return col, df.to_dict('records')
+    return col, df.to_json(date_format='iso', orient='split')
 
 
 if __name__ == '__main__':
